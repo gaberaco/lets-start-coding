45480c0ece36a3a12722d74a4cb5766ad21618c9
Patrick Raco <pataraco@gmail.com>
2020-04-19 19:54:14 -0700

    fix cpp linting

--- bowsers-theme.cpp
+++ bowsers-theme.cpp
@@ -4,45 +4,41 @@
  * and blink lights
  */
 
-void setup(){
-  pinMode(A5,OUTPUT); //Set up your speaker on pin A5 & the other pin in GND
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-} 
+// License: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>
+// Copyright (c) 2020 Gabriel Raco <gaberaco@gmail.com>
 
-void loop(){
+void setup() {
+  pinMode(A5, OUTPUT);  // Set up your speaker on pin A5 & the other pin in GND
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
 
-  tone(A5,300); //Send a tone of 250 hertz     
-  digitalWrite(12,HIGH);
+void loop() {
+  tone(A5, 300);  // Send a tone of 250 hertz
+  digitalWrite(12, HIGH);
   delay(100);
-  digitalWrite(12,LOW);
+  digitalWrite(12, LOW);
   noTone(A5);
   delay(100);
 
-  tone(A5,310); //Send a tone of 250 hertz     
-  digitalWrite(13,HIGH);
+  tone(A5, 310);  // Send a tone of 250 hertz
+  digitalWrite(13, HIGH);
   delay(100);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   noTone(A5);
   delay(100);
 
-  tone(A5,320); //Send a tone of 250 hertz     
-  digitalWrite(12,HIGH);
+  tone(A5, 320);  // Send a tone of 250 hertz
+  digitalWrite(12, HIGH);
   delay(100);
-  digitalWrite(12,LOW);
+  digitalWrite(12, LOW);
   noTone(A5);
   delay(100);
- 
-  tone(A5,310); //Send a tone of 250 hertz
-  digitalWrite(13,HIGH);
+
+  tone(A5, 310);  // Send a tone of 250 hertz
+  digitalWrite(13, HIGH);
   delay(100);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   noTone(A5);
   delay(100);
 }
-
-
-  
-
-
-// (c) 2018 Let's Start Coding. License: www.letsstartcoding.com/bsdlicense
